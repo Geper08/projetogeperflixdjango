c0e0ac96ead84bee914a290cbb4e2c27a609851a
Geovani Pereira <geovani.m.per@gmail.com>
2023-10-06 10:52:37 -0300

    update arquivos

--- geperflix/settings.py
+++ geperflix/settings.py
@@ -110,20 +110,20 @@ if DATABASE_URL:
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 AUTH_USER_MODEL = "filme.Usuario"
-# AUTH_PASSWORD_VALIDATORS = [
-#   {
-#     "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-#   },
-#   {
-#     "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-#   },
-#   {
-#     "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-#   },
-#   {
-#     "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
-#   },
-# ]
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
 
 
 # Internationalization
